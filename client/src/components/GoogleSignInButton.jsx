0c5ba6fb710bce7d8aee93ebe5b036f71fe2fb3c
ybhavaniprasad79 <ybhavaniprasad79@gmail.com>
2025-04-28 16:46:02 +0530

    all done

--- client/src/components/GoogleSignInButton.jsx
+++ client/src/components/GoogleSignInButton.jsx
@@ -2,7 +2,7 @@
   
   function GoogleSignInButton() {
     const handleGoogleSignIn = () => {
-        window.location.href = "http://localhost:5000/api/auth/google";
+        window.location.href = "https://eventhon.onrender.com/api/auth/google";
       };
     return (
         <button onClick={handleGoogleSignIn} className="bg-red-500 text-white px-4 py-2 rounded">
--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: "https://eventhon.netlify.app",
+    origin: "http://localhost:5173",
     credentials: true
 }));
 
