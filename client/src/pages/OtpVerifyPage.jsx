c4209a0fdd4d796edf4918f05232b5d07fe3f0c1
ybhavaniprasad79 <ybhavaniprasad79@gmail.com>
2025-04-28 23:58:05 +0530

    useEffect added

--- client/src/pages/OtpVerifyPage.jsx
+++ client/src/pages/OtpVerifyPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import axios from 'axios';
 import './OtpVerifyPage.css'; // import the CSS file
 import { useNavigate,useLocation } from 'react-router-dom';
